cc7449a2252a29f1c29ab7f214b78c983d601f07
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2022-12-05 00:51:59 +0100

    cleaned project

--- controllers/login.js
+++ controllers/login.js
@@ -1,46 +1,45 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/User')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/User");
 
-loginRouter.post('/', async (request, response) => {
+loginRouter.post("/", async (request, response) => {
   try {
-    const { body } = request
-    const { username, password } = body
-    const user = await User.findOne({ username })
+    const { body } = request;
+    const { username, password } = body;
+    const user = await User.findOne({ username });
 
-    const passwordCorrect = user === null
-      ? false
-      : await bcrypt.compare(password, user.passwordHash)
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    console.log("passwordCorrect : ", passwordCorrect);
 
     if (!(user && passwordCorrect)) {
       response.status(401).json({
-        error: 'invalid user or password'
-      })
+        error: "invalid user or password",
+      });
     }
 
     const userForToken = {
       id: user._id,
-      username: user.username
-    }
-
-    const token = jwt.sign(
-      userForToken,
-      process.env.SECRET,
-      {
-        expiresIn: 60 * 60 * 24 * 7
-      }
-    )
-
-    response.send({
-      name: user.name,
       username: user.username,
-      token
-    })
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET, {
+      expiresIn: 60 * 60 * 24 * 7,
+    });
+
+    const userOut = {
+      id: user._id,
+      username: user.username,
+      name: user.name,
+      type: user.type,
+      token,
+    };
+
+    response.status(201).json(userOut);
   } catch (e) {
-    console.log("error en login es : ", e)
+    console.log("error en login es : ", e);
   }
+});
 
-})
-
-module.exports = loginRouter
+module.exports = loginRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,59 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/User')
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/User");
+const userExtractor = require("../middleware/userExtractor");
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('notes', {
-    content: 1,
-    date: 1
-  })
-  response.json(users)
-})
+usersRouter.get("/", async (request, response) => {
+  try {
+    console.log(" Entro en GET users");
+    const users = await User.find({});
+    response.status(201).json(users);
+  } catch (error) {
+    return response.status(422).send(error);
+  }
+});
 
-usersRouter.post('/', async (request, response) => {
-  const { body } = request
-  const { username, name, password } = body
+usersRouter.post("/", async (request, response) => {
+  const { body } = request;
+  const { username, type, name, password } = body;
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
     username,
+    type,
     name,
-    passwordHash
-  })
+    passwordHash,
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
-})
+  response.status(201).json(savedUser);
+});
 
-module.exports = usersRouter
+usersRouter.put("/:id", userExtractor, (request, response, next) => {
+  try {
+    console.log(" Entro en PUT edit users");
+    const { id } = request.params;
+    const { username, name, type } = request.body;
+    const newUser = {
+      username,
+      name,
+      type,
+    };
+    User.findByIdAndUpdate(id, newUser, { new: true })
+      .then((result) => {
+        response.json(result);
+      })
+      .catch((error) => {
+        console.log("error User.findByIdAndUpdate", error);
+        return response.status(422).send(error);
+      });
+  } catch (error) {
+    console.log("error al update user ", error);
+    return response.status(422).send(error);
+  }
+});
+
+module.exports = usersRouter;
