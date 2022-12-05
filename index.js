c2a4ef5970630ba629e4fb57759c211b856662b6
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2022-12-05 01:43:05 +0100

    new commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("./mongo");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -20,6 +22,26 @@ app.use(cors());
 app.use(express.json());
 app.use("/images", express.static("images"));
 
+Sentry.init({
+  dsn: "https://ac034ebd99274911a8234148642e044c@o537348.ingest.sentry.io/5655435",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.get("/", (request, response) => {
   console.log(request.ip);
   console.log(request.ips);
@@ -31,6 +53,7 @@ app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
 app.use(notFound);
+app.use(Sentry.Handlers.errorHandler());
 app.use(handleErrors);
 
 const PORT = process.env.PORT || 3001;
